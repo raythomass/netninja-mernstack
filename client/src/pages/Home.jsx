0a9b388c6f5e6ff2441e6b56bb35b0dbed0b7b94
Ray Thomas <rthomas5678@gmail.com>
2024-08-26 18:54:36 -0700

    added workout context for page refresh

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,17 +2,20 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 
 export default function Home() {
-const [workouts, setWorkouts] = useState(null)
+  const {workouts, dispatch} = useWorkoutContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
         const response = await axios.get('/api/workouts');
         console.log(response);
         
-        setWorkouts(response.data)
+        if (response) {
+          dispatch({type: "SET_WORKOUTS", payload: response.data})
+        }
     };
 
     fetchWorkouts()
