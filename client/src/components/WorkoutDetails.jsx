82fc22921b181e657bebd82f834bc616f3051d4b
Ray Thomas <rthomas5678@gmail.com>
2024-08-27 11:42:39 -0700

    added finishing touches

--- client/src/components/WorkoutDetails.jsx
+++ client/src/components/WorkoutDetails.jsx
@@ -1,26 +1,28 @@
 import axios from 'axios'
 import { useWorkoutContext } from '../hooks/useWorkoutContext'
 
-export default function WorkoutDetails({ workout }) {
+//DATE FNS
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
+export default function WorkoutDetails({ workout }) {
   const { dispatch } = useWorkoutContext();
 
-const handleClick = async () => {
-  const response = await axios.delete('/api/workouts/' + workout._id) 
-  console.log(response)
+  const handleClick = async () => {
+    const response = await axios.delete('/api/workouts/' + workout._id) 
+    console.log(response)
 
-  if (response) {
-    dispatch({type: "DELETE_WORKOUT", payload: response.data})
-    // window.location.reload()
+    if (response) {
+      dispatch({type: "DELETE_WORKOUT", payload: response.data})
+      // window.location.reload()
+    }
   }
-}
 
   return (
     <div className="workout-details">
         <h4>{workout.title}</h4>
-        <p><strong>Load:</strong> {workout.load}</p>
+        <p><strong>Load (lb):</strong> {workout.load}</p>
         <p><strong>Reps:</strong> {workout.reps}</p>
-        <p>{workout.createdAt}</p>
+        <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true})}</p>
         <span onClick={handleClick}>Delete</span>
     </div>
   )
