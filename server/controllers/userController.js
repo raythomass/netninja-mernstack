5b582b361ea35a353b98b9724c00d7cfa0cac2e8
Ray Thomas <rthomas5678@gmail.com>
2024-09-30 15:42:28 -0700

    added routes for login and signup and tested them

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,25 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn:'3d'})
+}
 
 //LOGIN USER
 const loginUser = async (req, res) => {
-    res.json({mssg: 'Login User'})
+    const {email, password} = req.body
+    
+    try {
+        const user = await User.login(email, password)
+
+        //CREATE A TOKEN
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+        
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 //SIGNUP USER
@@ -11,13 +28,15 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+
+        //CREATE A TOKEN
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
         
     } catch (error) {
         res.status(400).json({error: error.message})
     }
-
-    res.json({mssg: 'Signup User'})
 }
 
 
